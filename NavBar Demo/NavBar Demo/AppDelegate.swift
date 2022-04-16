c2e8ffcbef8669fe37535a20096ba51146063be4
Mészáros Kristóf <meszaroskristof@Meszaros-MacBook-Air.local>
2022-04-16 17:05:50 +0200

    Trying out the methods to present View Controllers on Navigation View Controllers.

---
+++ NavBar Demo/NavBar Demo/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  NavBar Demo
+//
+//  Created by Mészáros Kristóf on 2022. 04. 16..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navigationController
+        
+        return true
+    }
+
+
+}
+
