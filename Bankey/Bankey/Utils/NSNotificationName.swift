6112e95789616d23f4ad5fd1da74c1734231420b
Mészáros Kristóf <meszaroskristof@Meszaros-MacBook-Air.local>
2022-04-21 12:35:15 +0200

    feat: Add logout button

---
+++ Bankey/Bankey/Utils/NSNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotificationName.swift
+//  Bankey
+//
+//  Created by Mészáros Kristóf on 2022. 04. 21..
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let logout = Notification.Name("Logout")
+}
---
+++ Bankey/Bankey/Utils/UITextField+SecureToggle.swift
@@ -0,0 +1,26 @@
+//
+//  UITextField+SecureToggle.swift
+//  Bankey
+//
+//  Created by Mészáros Kristóf on 2022. 04. 21..
+//
+
+import Foundation
+import UIKit
+
+let passwordToggleButton = UIButton(type: .custom)
+
+extension UITextField {
+    func enablePasswordToggle() {
+        passwordToggleButton.setImage(UIImage(systemName: "eye.fill"), for: .normal)
+        passwordToggleButton.setImage(UIImage(systemName: "eye.slash.fill"), for: .selected)
+        passwordToggleButton.addTarget(self, action: #selector(togglePasswordView), for: .touchUpInside)
+        rightView = passwordToggleButton
+        rightViewMode = .always
+    }
+    
+    @objc func togglePasswordView(_ sender: Any) {
+        isSecureTextEntry.toggle()
+        passwordToggleButton.isSelected.toggle()
+    }
+}
