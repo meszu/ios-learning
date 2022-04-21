f4a798a5f2e3063a6e3bfc6a3a47639332a6b85a
Mészáros Kristóf <meszaroskristof@Meszaros-MacBook-Air.local>
2022-04-21 15:27:12 +0200

    feat: Shake the bell

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -150,12 +150,12 @@ extension LoginViewController {
             return
         }
         
-        if username.isEmpty || password.isEmpty {
-            configureView(withMessage: "Username / password cannot be blank")
-            return
-        }
-        
-        if username == "Kevin" && password == "Welcome" {
+//        if username.isEmpty || password.isEmpty {
+//            configureView(withMessage: "Username / password cannot be blank")
+//            return
+//        }
+//
+        if username == "" && password == "" {
             signInButton.configuration?.showsActivityIndicator = true
             delegate?.didLogin()
         } else {
