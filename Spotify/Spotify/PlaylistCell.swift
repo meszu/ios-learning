efb3c65ad0125663d022f600461e1297925aea63
Mészáros Kristóf <meszaroskristof@Meszaros-MacBook-Air.local>
2022-05-09 12:16:47 +0200

    feat: add collection view

---
+++ Spotify/Spotify/PlaylistCell.swift
@@ -0,0 +1,14 @@
+//
+//  PlaylistsCell.swift
+//  Spotify
+//
+//  Created by Mészáros Kristóf on 2022. 05. 09..
+//
+
+import Foundation
+import UIKit
+
+class PlaylistCell: UICollectionViewCell {
+    
+    
+}
