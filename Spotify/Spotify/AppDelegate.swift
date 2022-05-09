654dea7dab2e18c46dd8c3bdd40f984cb77fa162
Mészáros Kristóf <meszaroskristof@Meszaros-MacBook-Air.local>
2022-05-09 11:30:29 +0200

    feat: set up navigation bar, container view

---
+++ Spotify/Spotify/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  Spotify
+//
+//  Created by Mészáros Kristóf on 2022. 05. 05..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .spotifyBlack
+        window?.makeKeyAndVisible()
+        
+        let navigatorController = UINavigationController(rootViewController: TitleBarController())
+        window?.rootViewController = navigatorController
+        
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().barTintColor = .spotifyBlack
+        
+        return true
+    }
+
+}
+
