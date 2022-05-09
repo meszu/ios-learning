cb64d1ad292de797a9da52fe5c3bc6be1c9fded8
Mészáros Kristóf <meszaroskristof@Meszaros-MacBook-Air.local>
2022-05-09 13:13:33 +0200

    feat: set up Menu Bar interactions

--- Spotify/Spotify/HomeController.swift
+++ Spotify/Spotify/HomeController.swift
@@ -35,6 +35,7 @@ class HomeController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .spotifyBlack
+        menuBar.delegate = self
         
         layout()
     }
@@ -80,5 +81,24 @@ extension HomeController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: collectionView.frame.height)
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+//        let index = targetContentOffset.pointee.x / view.frame.width
+//        menuBar.selectItem(at: Int(index))
+    }
+    
+    
+    // Scrolling - complex but beautiful
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        menuBar.scrollIndicator(to: scrollView.contentOffset)
+    }
+}
+
+extension HomeController: MenuBarDelegate {
+    
+    func didSelectItemAt(index: Int) {
+        let indexPath = IndexPath(item: index, section: 0)
+        collectionView.scrollToItem(at: indexPath, at: [], animated: true)
     }
 }
